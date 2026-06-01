019814ddb226e4dead652cfa1a76a33ed13167d8
Master2415 <duutabarquino2415@gmail.com>
2026-06-01 13:20:26 -0500

    ...

---
+++ config.js
@@ -0,0 +1,4 @@
+// config.js
+// Este archivo contiene la información sensible y no se subirá a GitHub.
+const SCRIPT_URL = 'https://script.google.com/a/macros/saludhorisoes.org/s/AKfycbwtlE_Td-UlDK3o5fNg9Qzq3cEBtYXgL9BxaarXvXVLz9JNHWTqb18bSQBeeSofHkb7/exec';
+// Puedes añadir más variables aquí si lo necesitas
